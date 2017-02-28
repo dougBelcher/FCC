a113a8409057894cedacf86d43994ae539cef8a0
Doug Belcher <ddbnull+git@gmail.com>
2017-02-28 12:53:28 -0600

    Update 09. Confirm the Ending.js

--- 06. Basic Algorithm Scripting/09. Confirm the Ending.js
+++ 06. Basic Algorithm Scripting/09. Confirm the Ending.js
@@ -1,6 +1,7 @@
-function end(str, target) {
+function confirmEnding(str, target) {
   var lastChar = str.substr(str.length-(target.length), target.length);
-  console.log(lastChar);
+  console.log("1. " +lastChar);
+  console.log("2. " +target);
   if(lastChar == target) {
     return true;
   } else {
@@ -8,7 +9,7 @@ function end(str, target) {
   }
 }
 
-//end('Bastian', 'n');
-//end('He has to give me a new name', 'name');
-//end('Connor', 'n');
-end('If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing', 'mountain');
+confirmEnding('Bastian', 'n');
+//confirmEnding('He has to give me a new name', 'name');
+//confirmEnding('Connor', 'n');
+//confirmEnding('If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing', 'mountain');
