1f002c973b0dbe80109a82dfc3a527ee05398f6a
Doug Belcher <ddbnull+git@gmail.com>
2017-02-28 11:58:03 -0600

    Update 08. Largest Number.js

--- 06. Basic Algorithm Scripting/08. Largest Number.js
+++ 06. Basic Algorithm Scripting/08. Largest Number.js
@@ -12,7 +12,7 @@ function largestOfFour(arr) {
     }
   }
   console.log(newArray);
-  return arr;
+  return newArray;
 }
 
 //largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
