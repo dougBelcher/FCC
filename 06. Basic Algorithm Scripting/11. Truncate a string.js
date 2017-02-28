ecda8b5f3e4aa7c5a7325ed2d0e082d29773dd9e
Doug Belcher <ddbnull+git@gmail.com>
2017-02-28 14:06:26 -0600

    Update 11. Truncate a string.js

--- 06. Basic Algorithm Scripting/11. Truncate a string.js
+++ 06. Basic Algorithm Scripting/11. Truncate a string.js
@@ -1,13 +1,25 @@
-function truncate(str, num) {
-  var newStr = str.slice(0, num-3);
+function truncateString(str, num) {
+  console.log("1. " + num);
+  if (num < str.length) {
+    if (num >3) {
+    num -= 3;
+    }
+  }
+  console.log("1.1 " + num);
+  var newStr = str.slice(0, num);
   if(num < str.length) {
     newStr = newStr + "...";
-    //console.log(newStr);
+    console.log("2. " + newStr);
     return newStr;
   } else {
-    //console.log(str);
+    console.log("3. " + str);
     return str;
   }
 }
 
-truncate('A-tisket a-tasket A green and yellow basket', 11);
+//truncateString('A-tisket a-tasket A green and yellow basket', 11);
+//truncateString('Peter Piper picked a peck of pickled peppers', 14);
+//truncateString('A-tisket a-tasket A green and yellow basket','A-tisket a-tasket A green and yellow basket'.length);
+//truncateString('A-tisket a-tasket A green and yellow basket', 'A-tisket a-tasket A green and yellow basket'.length + 2);
+truncateString('A-', 1);
+//truncateString('A-', 2);
