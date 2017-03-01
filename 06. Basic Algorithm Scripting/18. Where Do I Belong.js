c0968d1c41150078e67306c32a84477b83ab1f16
Doug Belcher <ddbnull+git@gmail.com>
2017-03-01 12:31:13 -0600

    Update 18. Where Do I Belong.js

--- 06. Basic Algorithm Scripting/18. Where Do I Belong.js
+++ 06. Basic Algorithm Scripting/18. Where Do I Belong.js
@@ -1,4 +1,4 @@
-function where(arr, num) {
+function getIndexToIns(arr, num) {
   // Push the number to be added onto the array
   arr.push(num);
   // Sort the array to get it in the correct order
@@ -15,6 +15,11 @@ function where(arr, num) {
   return i;
 }
 
-//where([20, 10, 60, 30, 40, 70], 50);
-//where([10, 20, 30, 40, 50], 35);
-where([10, 20, 30, 40, 50], 30);
+//getIndexToIns([20, 10, 60, 30, 40, 70], 50);
+//getIndexToIns([10, 20, 30, 40, 50], 35);
+getIndexToIns([10, 20, 30, 40, 50], 30);
+//getIndexToIns([40, 60], 50);
+//getIndexToIns([3, 10, 5], 3);
+//getIndexToIns([5, 3, 20, 3], 5);
+//getIndexToIns([2, 20, 10], 19);
+//getIndexToIns([2, 5, 10], 15);
